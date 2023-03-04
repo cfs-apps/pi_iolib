ac5b31c6afa8e638c4ada1009ad519a4eec81393
David McComas <dmccomas63@gmail.com>
2023-03-04 17:21:24 -0500

    Change config.h default to BCM2836_7 RPI 3B

--- config.h
+++ config.h
@@ -2,6 +2,6 @@
 ** One of the following BCM chips must be defined.
 */
 
-//#define BCM2836_7 1  // Pi 3B+
+#define BCM2836_7 1  // Pi 3B+
 //#define BCM2835   1  // Pi 
-#define BCM2711   1  // Pi 4
+//#define BCM2711   1  // Pi 4
